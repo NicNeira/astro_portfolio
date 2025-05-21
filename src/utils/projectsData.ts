785ad617f0f48a3c1d487c5af9fce35b95b06984
Nicolas Neira <neiranicolas17@gmail.com>
2025-05-21 19:49:41 -0400

    Refactor contenido del portafolio: eliminar datos de proyectos del archivo de slug y mover a un nuevo archivo projectsData.ts; actualizar lógica de contenido en la página principal.

---
+++ src/utils/projectsData.ts
@@ -0,0 +1,168 @@
+
+
+
+export const portfolioSectionTitle = "Elevating visual narratives";
+export const portfolioSectionSubtitle =
+  "Embark on a design journey that surpasses pixels, entering a realm of imagination. Explore my portfolio, where passion and creativity converge to shape enthralling visual narratives.";
+
+export const ContentProjects = [
+  {
+    id: "la-casa-de-hojas",
+    projectTitleHTML: "La Casa de Hojas :",
+    description: `"La Casa de Hojas" es una instalación artística que explora la historia de un individuo cuya casa crece de manera expansiva y descontrolada con el paso del tiempo. A medida que los años transcurren, el residente descubre nuevos pasillos, habitaciones y pisos, cada uno con estilos arquitectónicos notablemente distintos. Esta narrativa refleja el crecimiento descontrolado de una ciudad o la urbanización a través de la experiencia de este personaje que habita en un espacio extraño y en constante cambio.`,
+    href: "/portfolio/la-casa-de-hojas",
+    image: {
+      src: "https://picsum.photos/id/1018/1200/800",
+      alt: "Beautiful landscape with a lake and mountains",
+    },
+  },
+  {
+    id: "ciudad-latente",
+    projectTitleHTML: "Ciudad Latente:",
+    description: `A través de un recorrido contemplativo observamos como el olvidado humedal de batuco es amenazado por la urbanización y la invasión del hombre guiado por una persona que nació y se crío en el pueblo. Para retratar esta amenaza se hizo uso del simbolismo, a traves de la creación de una maqueta que representará la ciudad capitalista y diversos elementos que generarán contra punto visual.`,
+    href: "/portfolio/ciudad-latente",
+    image: {
+      src: "https://picsum.photos/id/1015/1200/800",
+      alt: "Sandy beach with turquoise water",
+    },
+  },
+  {
+    id: "inhumano",
+    projectTitleHTML: "Inhumano: <br />",
+    description: `El proyecto "Inhumano" es un cortometraje que planea mostrar lo más bajo del ser humano , por lo cual se usará un lenguaje visual enfocado en el ambiente y la atmósfera que produce la misma sobre el espectador . Para el lugar se barajaron varias ideas , pero se decidió por la idea más arriesgada , hacer la escenografía, reutilizando materiales , madera y pintura.
+
+        Para la ambientación se recurrió a materiales que dieran texturas pobre y vieja , se quería crear un ambiente descuidado y sucio. Tanto el espacio como la caracterización de los personajes , buscaban potenciar esta idea distopica y noir.`,
+    href: "/portfolio/inhumano",
+    image: {
+      src: "https://picsum.photos/id/1025/1200/800",
+      alt: "Cute pug dog looking at the camera",
+    },
+  },
+  {
+    id: "retrato-oval",
+    projectTitleHTML: "Retrato Oval:",
+    description: `Un estudiante de arcos busca contactar con el artista (Boisvert) que lo inspiro a dedicarse al cine y la animación, un viaje a la admiración que lentamente se transforma en obsesión con el paso de los días.
+
+        La escenografía está decorada por ilustraciones del artista mr.Doodle en combinación con las obras del mismismo Boisvert
+        El tratamiento se piensa en lo estético de la imagen, y en la importancia de este mundo que muchas veces se nos presenta en blanco y negro. Tanto la simbología de Boisvert y la estética de Doodle, profundizan con el relato, generándole forma y estructura visual acorde a lo que se quiere demostrar.`,
+    href: "/portfolio/retrato-oval",
+    image: {
+      src: "https://picsum.photos/id/1018/1200/800",
+      alt: "Beautiful landscape with a lake and mountains",
+    },
+  },
+  {
+    id: "inhumano-santiago",
+    projectTitleHTML:
+      'Inhumano: <br /><span class="text-2xl">En un Santiago desolador y decadente</span>',
+    description: `Redesigned the e-commerce website for an eco-conscious fashion brand, GreenVogue. The objective was to align the brand's online presence with its sustainable ethos and improve user experience.`,
+    href: "/portfolio/inhumano-santiago",
+    image: {
+      src: "https://picsum.photos/id/1040/1200/800",
+      alt: "Misty forest path in the morning",
+    },
+  },
+];
+
+export const SlugProjects = [
+  {
+    id: "la-casa-de-hojas",
+    projectTitle: "La Casa de Hojas",
+    description: `"La Casa de Hojas" es una instalación artística que explora la historia de un individuo cuya casa crece de manera expansiva y descontrolada con el paso del tiempo. A medida que los años transcurren, el residente descubre nuevos pasillos, habitaciones y pisos, cada uno con estilos arquitectónicos notablemente distintos. Esta narrativa refleja el crecimiento descontrolado de una ciudad o la urbanización a través de la experiencia de este personaje que habita en un espacio extraño y en constante cambio.`,
+    images: [
+      {
+        src: "https://picsum.photos/id/1018/1200/800",
+        alt: "Beautiful landscape with a lake and mountains",
+      },
+      {
+        src: "https://picsum.photos/id/1015/1200/800",
+        alt: "Sandy beach with turquoise water",
+      },
+      {
+        src: "https://picsum.photos/id/1025/1200/800",
+        alt: "Cute pug dog looking at the camera",
+      },
+    ],
+  },
+  {
+    id: "ciudad-latente",
+    projectTitle: "Ciudad Latente",
+    description: `A través de un recorrido contemplativo observamos como el olvidado humedal de batuco es amenazado por la urbanización y la invasión del hombre guiado por una persona que nació y se crío en el pueblo. Para retratar esta amenaza se hizo uso del simbolismo, a traves de la creación de una maqueta que representará la ciudad capitalista y diversos elementos que generarán contra punto visual.`,
+    images: [
+      {
+        src: "https://picsum.photos/id/1040/1200/800",
+        alt: "Misty forest path in the morning",
+      },
+      {
+        src: "https://picsum.photos/id/1018/1200/800",
+        alt: "Beautiful landscape with a lake and mountains",
+      },
+      {
+        src: "https://picsum.photos/id/1015/1200/800",
+        alt: "Sandy beach with turquoise water",
+      },
+    ],
+  },
+  {
+    id: "inhumano",
+    projectTitle: "Inhumano",
+    description: `El proyecto "Inhumano" es un cortometraje que planea mostrar lo más bajo del ser humano, por lo cual se usará un lenguaje visual enfocado en el ambiente y la atmósfera que produce la misma sobre el espectador. Para el lugar se barajaron varias ideas, pero se decidió por la idea más arriesgada, hacer la escenografía, reutilizando materiales, madera y pintura.
+
+        Para la ambientación se recurrió a materiales que dieran texturas pobre y vieja, se quería crear un ambiente descuidado y sucio. Tanto el espacio como la caracterización de los personajes, buscaban potenciar esta idea distopica y noir.`,
+    images: [
+      {
+        src: "https://picsum.photos/id/1025/1200/800",
+        alt: "Cute pug dog looking at the camera",
+      },
+      {
+        src: "https://picsum.photos/id/1040/1200/800",
+        alt: "Misty forest path in the morning",
+      },
+      {
+        src: "https://picsum.photos/id/1018/1200/800",
+        alt: "Beautiful landscape with a lake and mountains",
+      },
+    ],
+  },
+  {
+    id: "retrato-oval",
+    projectTitle: "Retrato Oval",
+    description: `Un estudiante de arcos busca contactar con el artista (Boisvert) que lo inspiro a dedicarse al cine y la animación, un viaje a la admiración que lentamente se transforma en obsesión con el paso de los días.
+
+        La escenografía está decorada por ilustraciones del artista mr.Doodle en combinación con las obras del mismismo Boisvert
+        El tratamiento se piensa en lo estético de la imagen, y en la importancia de este mundo que muchas veces se nos presenta en blanco y negro. Tanto la simbología de Boisvert y la estética de Doodle, profundizan con el relato, generándole forma y estructura visual acorde a lo que se quiere demostrar.`,
+    images: [
+      {
+        src: "https://picsum.photos/id/1015/1200/800",
+        alt: "Sandy beach with turquoise water",
+      },
+      {
+        src: "https://picsum.photos/id/1025/1200/800",
+        alt: "Cute pug dog looking at the camera",
+      },
+      {
+        src: "https://picsum.photos/id/1040/1200/800",
+        alt: "Misty forest path in the morning",
+      },
+    ],
+  },
+  {
+    id: "inhumano-santiago",
+    projectTitle: "Inhumano: En un Santiago desolador y decadente",
+    description: `Redesigned the e-commerce website for an eco-conscious fashion brand, GreenVogue. The objective was to align the brand's online presence with its sustainable ethos and improve user experience.`,
+    images: [
+      {
+        src: "https://picsum.photos/id/1018/1200/800",
+        alt: "Beautiful landscape with a lake and mountains",
+      },
+      {
+        src: "https://picsum.photos/id/1015/1200/800",
+        alt: "Sandy beach with turquoise water",
+      },
+      {
+        src: "https://picsum.photos/id/1025/1200/800",
+        alt: "Cute pug dog looking at the camera",
+      },
+    ],
+  },
+];
