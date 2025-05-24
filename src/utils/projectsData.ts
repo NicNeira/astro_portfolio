20c42683454d5fe9fb866832b4d044946927b296
Nicolas Neira <neiranicolas17@gmail.com>
2025-05-23 21:53:19 -0400

    Eliminar imágenes en formato JPG y reemplazarlas por versiones en formato WEBP para optimizar el rendimiento y la carga de la página. También se elimina el archivo BaseLayout.astro, que ya no es necesario.

--- src/utils/projectsData.ts
+++ src/utils/projectsData.ts
@@ -1,7 +1,111 @@
-import contentLeafHouse from "@assets/images/casaHojas/IMG_1010.jpg";
-import contentCityLatente from "@assets/images/ciudadLatente/IMG-20231125-WA0032.jpg";
-import contentInhumano from "@assets/images/inhumano/IMG_20240406_211940.jpg"
-import contentRetratoOval from "@assets/images/retratoOval/IMG-20221116-WA0011.jpg";
+import contentLeafHouse from "@assets/images/casaHojas/IMG_1010.webp";
+import contentCityLatente from "@assets/images/ciudadLatente/IMG-20231125-WA0032.webp";
+import contentInhumano from "@assets/images/inhumano/IMG_20240406_211940.webp"
+import contentRetratoOval from "@assets/images/retratoOval/IMG-20221116-WA0011.webp";
+
+
+import slugImageLaCasaDeHojas1 from "@assets/images/casaHojas/IMG_1010.webp";
+import slugImageLaCasaDeHojas2 from "@assets/images/casaHojas/IMG_1004.webp";
+import slugImageLaCasaDeHojas3 from "@assets/images/casaHojas/IMG_1056.webp";
+import slugImageLaCasaDeHojas4 from "@assets/images/casaHojas/IMG_1923.webp";
+
+import slugImageCiudadLatente1 from "@assets/images/ciudadLatente/IMG-20231125-WA0032.webp"; 
+import slugImageCiudadLatente2 from "@assets/images/ciudadLatente/IMG_20231007_161250.webp";
+import slugImageCiudadLatente3 from "@assets/images/ciudadLatente/IMG-20231122-WA0013.webp"; 
+import slugImageCiudadLatente4 from "@assets/images/ciudadLatente/image.webp";
+
+import slugImageInhumano1 from "@assets/images/inhumano/IMG_20230609_101302.webp";
+import slugImageInhumano2 from "@assets/images/inhumano/IMG_20240406_211940.webp";
+import slugImageInhumano3 from "@assets/images/inhumano/IMG_20230624_130203.webp";
+import slugImageInhumano4 from "@assets/images/inhumano/image.webp"; 
+import slugImageInhumano5 from "@assets/images/inhumano/IMG_20230706_181145.webp";
+
+import slugImageRetratoOval1 from "@assets/images/retratoOval/IMG-20221116-WA0011.webp";
+import slugImageRetratoOval2 from "@assets/images/retratoOval/IMG_20221022_160522.webp";
+import slugImageRetratoOval3 from "@assets/images/retratoOval/IMG_20221105_140500.webp";
+import slugImageRetratoOval4 from "@assets/images/retratoOval/IMG_20221017_214959.webp";
+
+const slugVideoInhumano = "https://www.youtube-nocookie.com/embed/WHOU6y4KiHo?si=gCuMb6C1rXqnsFGV";
+
+const slugImagesLaCasaDeHojas = [
+  {
+    src: slugImageLaCasaDeHojas1,
+    alt: "Beautiful landscape with a lake and mountains",
+  },
+  {
+    src: slugImageLaCasaDeHojas2,
+    alt: "Sandy beach with turquoise water",
+  },
+  {
+    src: slugImageLaCasaDeHojas3,
+    alt: "Cute pug dog looking at the camera",
+  },
+  {
+    src: slugImageLaCasaDeHojas4,
+    alt: "Misty forest path in the morning",
+  },
+];
+
+const slugImagesCiudadLatente = [
+  {
+    src: slugImageCiudadLatente1,
+    alt: "Beautiful landscape with a lake and mountains",
+  },
+  {
+    src: slugImageCiudadLatente2,
+    alt: "Sandy beach with turquoise water",
+  },
+  {
+    src: slugImageCiudadLatente3,
+    alt: "Cute pug dog looking at the camera",
+  },
+  {
+    src: slugImageCiudadLatente4,
+    alt: "Misty forest path in the morning",
+  },
+];
+
+const slugImagesInhumano = [
+  {
+    src: slugImageInhumano1,
+    alt: "Beautiful landscape with a lake and mountains",
+  },
+  {
+    src: slugImageInhumano2,
+    alt: "Sandy beach with turquoise water",
+  },
+  {
+    src: slugImageInhumano3,
+    alt: "Cute pug dog looking at the camera",
+  },
+  {
+    src: slugImageInhumano4,
+    alt: "Misty forest path in the morning",
+  },
+  {
+    src: slugImageInhumano5,
+    alt: "Misty forest path in the morning",
+  },
+];
+
+const slugImagesRetratoOval = [
+  {
+    src: slugImageRetratoOval1,
+    alt: "Beautiful landscape with a lake and mountains",
+  },
+  {
+    src: slugImageRetratoOval2,
+    alt: "Sandy beach with turquoise water",
+  },
+  {
+    src: slugImageRetratoOval3,
+    alt: "Cute pug dog looking at the camera",
+  },
+  {
+    src: slugImageRetratoOval4,
+    alt: "Misty forest path in the morning",
+  },
+];
 
 export const portfolioSectionTitle = "Elevando narrativas visuales";
 export const portfolioSectionSubtitle =
@@ -61,39 +165,13 @@ export const SlugProjects = [
     id: "la-casa-de-hojas",
     projectTitle: "La Casa de Hojas",
     description: `"La Casa de Hojas" es una instalación artística que explora la historia de un individuo cuya casa crece de manera expansiva y descontrolada con el paso del tiempo. A medida que los años transcurren, el residente descubre nuevos pasillos, habitaciones y pisos, cada uno con estilos arquitectónicos notablemente distintos. Esta narrativa refleja el crecimiento descontrolado de una ciudad o la urbanización a través de la experiencia de este personaje que habita en un espacio extraño y en constante cambio.`,
-    images: [
-      {
-        src: "https://picsum.photos/id/1018/1200/800",
-        alt: "Beautiful landscape with a lake and mountains",
-      },
-      {
-        src: "https://picsum.photos/id/1015/1200/800",
-        alt: "Sandy beach with turquoise water",
-      },
-      {
-        src: "https://picsum.photos/id/1025/1200/800",
-        alt: "Cute pug dog looking at the camera",
-      },
-    ],
+      images: slugImagesLaCasaDeHojas
   },
   {
     id: "ciudad-latente",
     projectTitle: "Ciudad Latente",
     description: `A través de un recorrido contemplativo observamos como el olvidado humedal de batuco es amenazado por la urbanización y la invasión del hombre guiado por una persona que nació y se crío en el pueblo. Para retratar esta amenaza se hizo uso del simbolismo, a traves de la creación de una maqueta que representará la ciudad capitalista y diversos elementos que generarán contra punto visual.`,
-    images: [
-      {
-        src: "https://picsum.photos/id/1040/1200/800",
-        alt: "Misty forest path in the morning",
-      },
-      {
-        src: "https://picsum.photos/id/1018/1200/800",
-        alt: "Beautiful landscape with a lake and mountains",
-      },
-      {
-        src: "https://picsum.photos/id/1015/1200/800",
-        alt: "Sandy beach with turquoise water",
-      },
-    ],
+    images: slugImagesCiudadLatente
   },
   {
     id: "inhumano",
@@ -101,20 +179,8 @@ export const SlugProjects = [
     description: `El proyecto "Inhumano" es un cortometraje que planea mostrar lo más bajo del ser humano, por lo cual se usará un lenguaje visual enfocado en el ambiente y la atmósfera que produce la misma sobre el espectador. Para el lugar se barajaron varias ideas, pero se decidió por la idea más arriesgada, hacer la escenografía, reutilizando materiales, madera y pintura.
 
         Para la ambientación se recurrió a materiales que dieran texturas pobre y vieja, se quería crear un ambiente descuidado y sucio. Tanto el espacio como la caracterización de los personajes, buscaban potenciar esta idea distopica y noir.`,
-    images: [
-      {
-        src: "https://picsum.photos/id/1025/1200/800",
-        alt: "Cute pug dog looking at the camera",
-      },
-      {
-        src: "https://picsum.photos/id/1040/1200/800",
-        alt: "Misty forest path in the morning",
-      },
-      {
-        src: "https://picsum.photos/id/1018/1200/800",
-        alt: "Beautiful landscape with a lake and mountains",
-      },
-    ],
+    images: slugImagesInhumano,
+    video: slugVideoInhumano
   },
   {
     id: "retrato-oval",
@@ -123,20 +189,6 @@ export const SlugProjects = [
 
         La escenografía está decorada por ilustraciones del artista mr.Doodle en combinación con las obras del mismismo Boisvert
         El tratamiento se piensa en lo estético de la imagen, y en la importancia de este mundo que muchas veces se nos presenta en blanco y negro. Tanto la simbología de Boisvert y la estética de Doodle, profundizan con el relato, generándole forma y estructura visual acorde a lo que se quiere demostrar.`,
-    images: [
-      {
-        src: "https://picsum.photos/id/1015/1200/800",
-        alt: "Sandy beach with turquoise water",
-      },
-      {
-        src: "https://picsum.photos/id/1025/1200/800",
-        alt: "Cute pug dog looking at the camera",
-      },
-      {
-        src: "https://picsum.photos/id/1040/1200/800",
-        alt: "Misty forest path in the morning",
-      },
-    ],
+    images: slugImagesRetratoOval
   },
-  
 ];
